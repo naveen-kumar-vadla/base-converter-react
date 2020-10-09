073af5d7d6fbbe3250a351792282fd969c532a02
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-10 02:18:49 +0530

    Added base16 conversion

--- src/BaseConversion.js
+++ src/BaseConversion.js
@@ -20,33 +20,51 @@ const LabelWithInput = ({ text, value, onChange }) => {
 class BaseConversion extends Component {
   constructor(props) {
     super(props);
-    this.state = { base2Value: 1001, base8Value: 11, base10Value: 9 };
+    this.state = {
+      base2Value: '1010',
+      base8Value: '12',
+      base10Value: '10',
+      base16Value: 'a',
+    };
     this.onBase2Change = this.onBase2Change.bind(this);
     this.onBase8Change = this.onBase8Change.bind(this);
     this.onBase10Change = this.onBase10Change.bind(this);
+    this.onBase16Change = this.onBase16Change.bind(this);
   }
 
   onBase2Change(value) {
     this.setState(_ => ({
       base2Value: value,
-      base8Value: BaseConvertor.binaryToOctal(value) || '',
-      base10Value: BaseConvertor.binaryToDecimal(value) || '',
+      base8Value: BaseConvertor.binaryToOctal(value),
+      base10Value: BaseConvertor.binaryToDecimal(value),
+      base16Value: BaseConvertor.binaryToHexaDecimal(value),
     }));
   }
 
   onBase8Change(value) {
     this.setState(_ => ({
-      base2Value: BaseConvertor.octalToBinary(value) || '',
+      base2Value: BaseConvertor.octalToBinary(value),
       base8Value: value,
-      base10Value: BaseConvertor.octalToDecimal(value) || '',
+      base10Value: BaseConvertor.octalToDecimal(value),
+      base16Value: BaseConvertor.octalToHexDecimal(value),
     }));
   }
 
   onBase10Change(value) {
     this.setState(_ => ({
-      base2Value: BaseConvertor.decimalToBinary(value) || '',
-      base8Value: BaseConvertor.decimalToOctal(value) || '',
+      base2Value: BaseConvertor.decimalToBinary(value),
+      base8Value: BaseConvertor.decimalToOctal(value),
       base10Value: value,
+      base16Value: BaseConvertor.decimalToHexaDecimal(value),
+    }));
+  }
+
+  onBase16Change(value) {
+    this.setState(_ => ({
+      base2Value: BaseConvertor.hexaDecimalToBinary(value),
+      base8Value: BaseConvertor.hexaDecimalToOctal(value),
+      base10Value: BaseConvertor.hexaDecimalToDecimal(value),
+      base16Value: value,
     }));
   }
 
@@ -67,6 +85,11 @@ class BaseConversion extends Component {
           text='Base 10'
           value={this.state.base10Value}
           onChange={this.onBase10Change}
+        />
+        <LabelWithInput
+          text='Base 16'
+          value={this.state.base16Value}
+          onChange={this.onBase16Change}
         />
       </div>
     );
